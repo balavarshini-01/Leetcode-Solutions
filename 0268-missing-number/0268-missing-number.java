f5d33e97068aebc53d2892b0d153348b11e87e14
LOGESWARI S <102917079+balavarshini-01@users.noreply.github.com>
2025-03-02 22:39:04 +0530

    Time: 2 ms (27.83%), Space: 45.3 MB (54.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -10,6 +10,9 @@ class Solution {
         //     return len;
         // }
    int n=nums.length;
-       return (IntStream.range(0,n+1)).sum() - Arrays.stream(nums).sum()  ;
+       //return (IntStream.range(0,n+1)).sum() - Arrays.stream(nums).sum()  ;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = Arrays.stream(nums).sum();
+        return expectedSum - actualSum;
     }
 }
